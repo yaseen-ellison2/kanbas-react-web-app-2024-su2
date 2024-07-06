c0702c090d93c2f528e5fac8a9ced45adc92f7ec
yaseen-ellison2 <129689150+yaseen-ellison2@users.noreply.github.com>
2024-07-06 14:19:27 -0700

    Initial Commit For Kanbas

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Labs from './Labs';
+
+function App() {
+  return (
+    <div>
+      <h1> Welcome to Web Dev</h1>
+      <h2>Yaseen Ellison</h2>
+      <Labs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Labs/Lab2/index.tsx
@@ -0,0 +1,7 @@
+export default function Lab2() {
+    return (
+      <div id="wd-lab2">
+        <h2>Lab 2</h2>
+      </div>
+    );
+  }
