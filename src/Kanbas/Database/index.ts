a7bca7b2356ae4557f01b99045135af22702efb0
yaseen-ellison2 <129689150+yaseen-ellison2@users.noreply.github.com>
2024-07-21 23:19:00 -0700

    Data Driven Assignment Screen and Subsequent Styling Issues Sorted

--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -1,4 +1,10 @@
 import courses from "./courses.json";
-import modules from "./modules.json"
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import grades from "./grades.json";
+import users from "./users.json";
 
-export { courses, modules };
+
+
+
+export { courses, modules, assignments, grades, users };
